cd9afc1eca63656a45806a07ca3e69686f4e196a
alexander-jimenez11 <gajimenez2@utpl.edu.ec>
2021-04-29 16:51:03 -0500

    Tarea

---
+++ problema1/Problema1/src/problema1/Ejecutor.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problema1;
+
+/**
+ *
+ * @author Alexander
+ */
+public class Ejecutor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = "Alexander Jiménez";
+        double calificacion1 = 9.8;
+        double calificacion2 = 9.9;
+        double calificacion3 = 10.0;
+        Estudiante est1 = new Estudiante(nombre, calificacion1, calificacion2,
+                calificacion3);
+        est1.establecerPromedioCalificaciones();
+        est1.obtenerCalificacionMateria1();
+        est1.obtenerCalificacionMateria2();
+        est1.obtenerCalificacionMateria3();
+        //est1.obtenerPromedioCalificaciones();
+
+        System.out.printf("%s\n", est1);
+
+        String nombreEstudiante = "Gimi Medina";
+        double nota1 = 9.8;
+        double nota2 = 8.8;
+        double nota3 = 6.8;
+        Estudiante est2 = new Estudiante(nombreEstudiante, nota1, nota2, nota3);
+        est2.establecerPromedioCalificaciones();
+        est2.obtenerCalificacionMateria1();
+        est2.obtenerCalificacionMateria2();
+        est2.obtenerCalificacionMateria3();
+        //est2.obtenerPromedioCalificaciones();
+
+        System.out.printf("%s\n", est2);
+    }
+
+}
